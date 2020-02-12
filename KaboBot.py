7dfce39a12090fa118bebe6f45924d835009528f
Danilsuper2 <danil.mironov.1029@gmail.com>
2020-02-12 21:50:15 +0700

    Test inline keyboard

--- KaboBot.py
+++ KaboBot.py
@@ -1,7 +1,32 @@
 import telebot
+from telebot import types
 from Kabo import token, chat_id
 
 telebot.apihelper.proxy = {'https': 'socks5h://geek:socks@t.geekclass.ru:7777'}
 
 kabo = telebot.TeleBot(token=token)
-# kabo.send_message(chat_id, 'Чай?')
+
+
+@kabo.message_handler()
+def f(message):
+    if message.chat.id == 542658693:  # TG ID Кабо
+        keyboard = types.InlineKeyboardMarkup()
+        keyboard.add(types.InlineKeyboardButton(text='1', callback_data=1))
+        keyboard.add(types.InlineKeyboardButton(text='2', callback_data=2))
+        keyboard.add(types.InlineKeyboardButton(text='3', callback_data=3))
+        kabo.send_message(message.chat.id, 'Какие люди!\nЧего вы желаете?',
+                          reply_markup=keyboard)
+    else:
+        keyboard = types.InlineKeyboardMarkup()
+        keyboard.add(types.InlineKeyboardButton(text='1', callback_data=1))
+        keyboard.add(types.InlineKeyboardButton(text='2', callback_data=2))
+        keyboard.add(types.InlineKeyboardButton(text='3', callback_data=3))
+        kabo.send_message(message.chat.id, 'Привет! Я бот чаёк.\n\nПрошу ждать оповещения в беседе.')
+    # print(message)
+
+
+@kabo.callback_query_handler(func=lambda call: True)
+def f(call):
+    kabo.send_message(call.message.chat.id, 'Вы нажали на кнопку {}'.format(call.data))
+
+kabo.polling(none_stop=True)
