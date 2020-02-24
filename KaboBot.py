1b5e455a375e9feb9a6b6778ba4c7c9610b22deb
Danilsuper2 <danil.mironov.1029@gmail.com>
2020-02-24 18:31:46 +0700

    menu, keyboard

--- KaboBot.py
+++ KaboBot.py
@@ -2,6 +2,14 @@ import telebot
 from telebot import types
 from Kabo import token, chat_id
 
+
+def k(buttons):
+    keyboard = types.InlineKeyboardMarkup()
+    for button in buttons:
+        keyboard.add(types.InlineKeyboardButton(**button))
+    return keyboard
+
+
 telebot.apihelper.proxy = {'https': 'socks5h://geek:socks@t.geekclass.ru:7777'}
 
 kabo = telebot.TeleBot(token=token)
@@ -11,23 +19,23 @@ kabo = telebot.TeleBot(token=token)
 def f(message):
     if message.chat.id == 542658693:  # TG ID Кабо
         keyboard = types.InlineKeyboardMarkup()
-        keyboard.add(types.InlineKeyboardButton(text='1', callback_data=1))
-        keyboard.add(types.InlineKeyboardButton(text='2', callback_data=2))
-        keyboard.add(types.InlineKeyboardButton(text='3', callback_data=3))
         kabo.send_message(message.chat.id, 'Какие люди!\nЧего вы желаете?',
-                          reply_markup=keyboard)
+                          reply_markup=k([
+                              {'text': 'Распределение по чайкам', 'callback_data': 'tea'},
+                              {'text': 'none', 'callback_data': 'none'},
+                              {'text': 'none', 'callback_data': 'none'},
+                          ]))
     else:
-        keyboard = types.InlineKeyboardMarkup()
-        keyboard.add(types.InlineKeyboardButton(text='1', callback_data=1))
-        keyboard.add(types.InlineKeyboardButton(text='2', callback_data=2))
-        keyboard.add(types.InlineKeyboardButton(text='3', callback_data=3))
         kabo.send_message(message.chat.id, 'Привет! Я бот чаёк.\n\nПрошу ждать оповещения в беседе.')
     # print(message)
 
 
 @kabo.callback_query_handler(func=lambda call: True)
 def f(call):
-    kabo.send_message(call.message.chat.id, 'Вы нажали на кнопку {}'.format(call.data))
+    if call.data == 'tea':
+        kabo.send_message(call.message.chat.id, 'Начинаю распределение по чайкам!')
+    elif call.data == 'none':
+        kabo.send_message(call.message.chat.id, 'Ничего нет)')
 
 
-kabo.polling(none_stop=True)
+kabo.polling(none_stop=True, timeout=100)
